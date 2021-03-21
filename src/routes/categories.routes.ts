6d99a4f836aa1bdf70f15e9bc1765d69a7ba5c1c
Guilherme Henrique <guilherme.alvs@hotmail.com>
2021-03-20 21:37:13 -0300

    Singleton Pattern

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,30 +1,16 @@
 import { Router } from "express";
 
-import CategoriesRepository from "../modules/cars/repositories/CategoriesRepository";
-import CreateCategoryService from "../modules/cars/services/CreateCategoryService";
+import { createCategoryController } from "../modules/cars/useCases/createCategory/index";
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
 
-const categoriesRepository = new CategoriesRepository();
-
 categoriesRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-  try {
-    createCategoryService.execute({ name, description });
-  } catch (err) {
-    return response.status(400).json({ error: "Category Already exists!" });
-  }
-
-  return response.status(201).send();
+  return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
-  const categories = categoriesRepository.list();
-
-  return response.status(200).json({ categories });
+  return listCategoriesController.handle(request, response);
 });
 
 export default categoriesRoutes;
